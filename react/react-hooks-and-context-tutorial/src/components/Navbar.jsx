492d5eed789395e6024c00f763b884c5318de57a
Misha Kessler <misha.kessler@gmail.com>
2020-11-11 18:54:46 -0500

    Add class component contexts via consumer and static import.

---
+++ react/react-hooks-and-context-tutorial/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <ThemeContext.Consumer>{(context) => {
+        const { isDarkTheme, darkTheme, lightTheme } = context;
+        const theme = isDarkTheme ? darkTheme : lightTheme;
+        return (
+          <nav style={{ background: theme.background, color: theme.text, height: '120px'}}>
+            <h2 style={{ textAlign: 'center', }}>
+              Oak Academy
+            </h2>
+            <p
+              style={{ textAlign: 'center' }}
+            >
+            </p>
+            <div className='ui three buttons'>
+              <button className='ui button'>Overview</button>
+              <button className='ui button'>Contact</button>
+              <button className='ui button'>Support</button>
+            </div>
+          </nav>
+        )
+        }}
+
+        </ThemeContext.Consumer>
+    );
+  };
+}
+
+export default Navbar;
--- react/react-hooks-and-context-tutorial/src/components/TodoList.js
+++ react/react-hooks-and-context-tutorial/src/components/TodoList.js
@@ -1,41 +1,29 @@
 import React, { useContext, useState } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
-const TodoList = () => {
-	const [todo, setTodo] = useState('');
+class TodoList extends React.Component {
+  static contextType = ThemeContext;
 
-	const handleChange = e => {
-		setTodo(e.target.value);
-	};
+  render() {
+    const { isDarkTheme, darkTheme, lightTheme } = this.context
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+    
+    return (
+      <div style={{
+        background: theme.background,
+        color: theme.text,
+      }}>
 
-	const handleFormSubmit = e => {
-		e.preventDefault();
-		// dispatch({ type: 'ADD_TODO', text: todo });
-	};
 
-	const handleRemoveTodo = e => {
-		const id = e.target.id;
-		// dispatch({ type: 'REMOVE_TODO', id });
-	};
-
-	return (
-		<div>
-			{/* {
-				todos.length ? (
-					todos.map(todo => {
-						return <p key={todo.id} id={todo.id} onClick={handleRemoveTodo} className='item'>{todo.text}</p>
-					}) 
-				) : (
-					<div>You have no todos</div>
-				)
-			} */}
-			<form onSubmit={handleFormSubmit} >
-				<label htmlFor='todo' >Add todo:</label>
-				<input type='text' id='todo' onChange={handleChange} />
-				<input className='ui button primary' type='submit' value='Add new todo' style={{ marginRight: '4px' }} />
-				<button className='ui button primary'>change the theme</button>
-			</form>
-		</div>
-	)
-};
+        <form>
+          <label htmlFor='todo' >Add todo:</label>
+          <input type='text' id='todo'/>
+          <input className='ui button primary' type='submit' value='Add new todo' style={{ marginRight: '4px' }} />
+          <button className='ui button primary'>change the theme</button>
+        </form>
+      </div>
+    )
+  };
+}
 
 export default TodoList;
