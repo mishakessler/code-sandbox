82f996d1f723bcd622a929fde1d9b8e927ed7f7c
Misha Kessler <misha.kessler@gmail.com>
2021-01-25 21:18:09 -0500

    (generic commit) Add all changes

---
+++ react/react-comp-v-element/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const reactElement = <div>Hello!</div>;
+
+// const reactComponentNoJSX = () => React.createElement('div', null, 'Hello!');
+
+// const reactComponentJSX = () => {
+//   return <div>Hello!</div>;
+// }
+
+
+const domElement = document.getElementById('root')
+
+ReactDOM.render(Element, domElement)
